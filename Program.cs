d1d7d42c19b804a9d461204a26fbe20e4b608521
Carliertheo <carlier85640@gmail.com>
2021-04-05 23:41:20 +0200

    nouvelmodif

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace jeux_du_gobelet
         }
         
         // Variable pour la mise 
-        public int  Mise 
+        public int Mise 
         {
             get; set;
         }
@@ -40,12 +40,92 @@ namespace jeux_du_gobelet
         }
     }
 
+    
+    class Gobelet
+    {
+        private bool Jeton = false;
+
+        public Gobelet()
+        {
+            Jeton = false;
+        }
+    }
+
+    class Jeu
+    {
+        int[] tableau_joueur = new int[5];
+        int[] tableau_gobelet = new int[5];
+        
+        public Jeu (int nombre_joueur, int nombre_gobelet)
+        {
+            this.tableau_gobelet = new Gobelet [nombre_gobelet];
+            int win = new Random().Next(0, nombre_gobelet);
+
+            for (int i=0; i<nombre_gobelet; i++)
+            {
+                this.gobelet[i] = i == win ? Gobelet.gagnant() : Gobelet.vide();
+            }
+
+            this.joueur = new Joueur [nombre_joueur];
+            this.humain = new Random().Next(0, nombre_joueur);
+            
+            for (int i = 0; i < nombre_joueur; i++)
+            {
+                if (i != humain)
+                {
+                    this.Joueur[i] = new Joueur("Robot " + i.ToString(), new Random().Next(1, mise_max), new Random().Next(0, nombre_gobelet));
+                }
+                else
+                {
+                    this.joueur[i] = new Joueur("Temp", 0, -1);
+                }
+            }
+
+            Console.WriteLine("Le montnatn du gro slot est de  " + GrosLot().ToString());
+            Console.WriteLine("Paris par gobelets : " + StringParisParGobelets());
+            
+            int humain_mise = -1;
+            int humain_gobelet = -1;
+
+            Console.WriteLine("Quel est votre nom ?");
+            string humain_nom = Console.ReadLine();
+
+            while (humain_mise <0 || humain_mise > mise_max)
+            {
+                Console.WriteLine("Saisir votre mise: ");
+                humain_mise = Convert.ToInt32(Console.ReadLine());
+            } 
+
+            while (humain_gobelet <0 || humain_gobelet > nombre_gobelet-1)
+            {
+                Console.WriteLine("choisir un gobelet: ");
+                humain_gobelet = Convert.ToInt32(Console.ReadLine());
+            } 
+
+            this.joueur[huamin] = new Joueur(humain_nom, humain_mise, humain_gobelet);
+
+            
+
+    
+        }
+        public void Resultat()
+        {
+            bool [] gagnants = Gagnants();
+            int [] parisârgobelet = ParisParGobelets();
+            int gobelet_gagnants = gobelet_gagnants();
+            int gain = GrosLot()/TotalParisGagnants();
+
+            Console.WriteLine("Les gagnants ont joué le gobelet : " + gobelets_gagnant.ToString());
+            
+        }
+    }
+
     class Program
     {
         static void Main()
         {
-            Joueur joueur1 = new Joueur("theo", 10, 5);
-            Console.WriteLine("joueur1 Name = {0} Mise = {1} Gobelet = {3}", joueur1.Name, joueur1.Mise, joueur1.Gobelet);
+            Joueur joueur1 = new Joueur("Theo", 10, 5);
+            Console.WriteLine("le joueur 1 s'appelle = {0} et mise = {1} sur le Gobelet = {2}", joueur1.Name, joueur1.Mise, joueur1.Gobelet);
 
             //On déclare un deuxieme joueur et on lui affect les paramètre du joueur numero 1 
             Joueur joueur2 = joueur1;
@@ -54,8 +134,7 @@ namespace jeux_du_gobelet
             joueur2.Mise = 40;
             joueur2.Gobelet = 4;
 
-            Console.WriteLine("joueur2 Name = {0} Mise = {1} Gobelet = {3}", joueur2.Name, joueur2.Mise, joueur2.Gobelet);
-            Console.WriteLine("joueur1 Name = {0} Mise = {1} Gobelet = {3}", joueur1.Name, joueur1.Mise, joueur1.Gobelet);
+            Console.WriteLine("le joueur 2 s'appelle = {0} et mise = {1} sur le Gobelet = {2}", joueur2.Name, joueur2.Mise, joueur2.Gobelet);
 
             Console.WriteLine("Appuyer sur une touche pour fermer le programme");
             Console.ReadKey();
--- README.md
+++ README.md
@@ -3,16 +3,42 @@
 ## Sommaire : 
  
 [1 - Classe joueur](#classejoueur)<br/>
-[2 - Implémentation de la classe Gobelet](###test)<br/>
+[2 - Implémentation de la classe Gobelet](#gobelet)<br/>
 [3 - Implémentation de la classe Jeu](###test)
 
 ***
 
-## <a id="classejoueur" /> Classe joueur
+## <a id="classejoueur" /> Classe joueur</a>
 [1 - Implémentation](#implementation)<br/>
 [2 - Utilisation](#utilisation)<br/>
 
-## <a id="implementation" /> Implémentation de la classe joueur
+## <a id="implementation" /> Implémentation de la classe joueur</a>
 ![Image du code csharp](/img/image_classe_joueur.png)
 
-## <a id="utilisation" /> Utilisation de la classe joueur
+***
+
+
+## <a id="utilisation" /> Utilisation de la classe joueur</a>
+[1 - Instancier un objet de cette classe](#objetclasse)<br/>
+[2 - Acceder a un attribut de ce nouvel objet et afficher en le contenu dans un terminal en utilisant la fonction Console.WriteLine()](#attributconsole)<br/>
+[3 - Affecter une nouvelle valeur à cette attribut](#valeurattribut)<br/>
+[4 - Comment peut empêcher la lecture ou l'écriture dans un attribut](#lectureecriture)<br/>
+[5 - Quel est la différence entre un champs et une propriété en C# ?](#champspropriete)<br/>
+[6 - Donner un exemple d'utilisation de l'un et de l'autre en expliquant ce qu'est un accesseur](#accesseur)<br/>
+
+
+## <a id="objetclasse" /> Instancier un objet de cette classe</a>
+![Image du code csharp](/img/objet.png)
+
+## <a id="attributconsole" /> Acceder a un attribut de ce nouvel objet et afficher en le contenu dans un terminal en utilisant la fonction Console.WriteLine()</a>
+![Image du code csharp](/img/cmd.png)
+
+## <a id="champspropriete" /> Quel est la différence entre un champs et une propriété en C# ?</a>
+Un champs est un membre d'une classe, il est comme une variable d'un objet, il est généralement déclaré en private.
+
+## <a id="accesseur" /> Donner un exemple d'utilisation de l'un et de l'autre en expliquant ce qu'est un accesseur</a>
+Les proprietes sont en fait des methodes speciales qu'on appelle des accesseurs, ces accesseurs sont un moyen de recuperer et d'attribuer la valeur d'un champs qui est cense etre en private
+
+***
+
+## <a id="gobelet" /> Implémentation de la classe Gobelet </a>
